bf1f3d15d2c15f321cc3bab9727e9e7e02c27d3a
Matias Seijas <matiseijas@gmail.com>
2015-01-31 19:26:48 -0500

    Chapter 2 - In Progress ;)

---
+++ textbook_work/matias_seijas/Chapter 2/RandomItems/RandomItems/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Matias Seijas on 1/31/15.
+//  Copyright (c) 2015 Matias Seijas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        // Send the message addObject: to the NSMutableArray pointed to
+        // by the variable items, passing a string each time
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // Send another message, insertObjet:atIndex:, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // For every item in the items array...
+        for (NSString *item in items) {
+            // Log the description of item
+            NSLog(@"%@", item);
+        }
+        
+        // Destroy the mutable array object
+        items = nil;
+        
+    }
+    return 0;
+}
