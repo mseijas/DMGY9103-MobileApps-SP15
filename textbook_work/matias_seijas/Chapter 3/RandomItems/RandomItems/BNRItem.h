991772e88be692b4d821b58c4aad043f2e7bf3b6
Matias Seijas <matiseijas@gmail.com>
2015-02-07 17:29:30 -0500

    Chapter 3

---
+++ textbook_work/matias_seijas/Chapter 3/RandomItems/RandomItems/BNRItem.h
@@ -0,0 +1,46 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Matias Seijas on 1/31/15.
+//  Copyright (c) 2015 Matias Seijas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// ** Instance variables **
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
+// ** Class Methods **
++ (instancetype)randomItem;
+
+
+// ** Instance Methods **
+// Designated initializer for BNRItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+- (instancetype)initWithItemName:(NSString *)name
+                    serialNumber:(NSString *)sNumber;
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
