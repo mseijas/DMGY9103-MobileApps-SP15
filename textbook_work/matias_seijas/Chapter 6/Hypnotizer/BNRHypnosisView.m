610189cbc53ed4641d064c965f9e71a6fa1a3a41
Matias Seijas <matiseijas@gmail.com>
2015-02-14 16:19:02 -0500

    Chapter 6 - In Progress

--- textbook_work/matias_seijas/Chapter 6/Hypnotizer/BNRHypnosisView.m
+++ textbook_work/matias_seijas/Chapter 6/Hypnotizer/BNRHypnosisView.m
@@ -8,6 +8,13 @@
 
 #import "BNRHypnosisView.h"
 
+@interface BNRHypnosisView ()
+
+@property (strong, nonatomic) UIColor *circleColor;
+
+@end
+
+
 @implementation BNRHypnosisView
 
  
@@ -43,7 +50,7 @@
     path.lineWidth = 10;
     
     // Configure the drawing color to light gray
-    [[UIColor lightGrayColor] setStroke];
+    [self.circleColor setStroke];
     
     // Draw the line!
     [path stroke];
@@ -107,9 +114,7 @@
     CGContextSetShadow(currentContext, CGSizeMake(4, 7), 3);
     
     UIImage *logoImage = [UIImage imageNamed:@"logo.png"];
-    CGRect myRect = CGRectMake(100, 100, 100, 100);
-    
-    [logoImage drawInRect:myRect];
+    [logoImage drawInRect:CGRectMake(bounds.size.width / 4, bounds.size.height / 4, bounds.size.width / 2, bounds.size.height / 2)];
     
     CGContextRestoreGState(currentContext);
     
@@ -122,9 +127,29 @@
     if (self) {
         // All BNRHypnosisViews start with a clear background color
         self.backgroundColor = [UIColor clearColor];
+        self.circleColor = [UIColor lightGrayColor];
     }
     return self;
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"%@ was touched", self);
+    
+    // Get 3 random numbers between 0 and 1
+    float red = (arc4random() % 100) / 100.0;
+    float green = (arc4random() % 100 / 100.0);
+    float blue = (arc4random() % 100) / 100.0;
+    
+    UIColor *randomColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    
+    self.circleColor = randomColor;
+}
+
+- (void)setCircleColor:(UIColor *)circleColor
+{
+    _circleColor = circleColor;
+    [self setNeedsDisplay];
+}
 
 @end
