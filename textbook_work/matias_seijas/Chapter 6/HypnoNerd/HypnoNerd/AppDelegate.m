99f63f2b0496912c045bb4c4632ca19ba1576fab
Matias Seijas <matiseijas@gmail.com>
2015-02-14 17:42:54 -0500

    Chapter 6 - In Progress

--- textbook_work/matias_seijas/Chapter 6/HypnoNerd/HypnoNerd/AppDelegate.m
+++ textbook_work/matias_seijas/Chapter 6/HypnoNerd/HypnoNerd/AppDelegate.m
@@ -24,11 +24,12 @@
     BNRHypnosisViewController *hvc = [[BNRHypnosisViewController alloc] init];
     
     // This will get a pointer to an object that represents the app bundle
-    NSBundle *appBundle = [NSBundle mainBundle];
+    // NSBundle *appBundle = [NSBundle mainBundle];
     
     // Look in the appBundle for the file BNRReminderViewController.xib
-    BNRReminderViewController *rvc = [[BNRReminderViewController alloc] initWithNibName:@"BNRReminderViewController" bundle:appBundle];
+    // BNRReminderViewController *rvc = [[BNRReminderViewController alloc] initWithNibName:@"BNRReminderViewController" bundle:appBundle];
     
+    BNRReminderViewController *rvc = [[BNRReminderViewController alloc] init];
     
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
     tabBarController.viewControllers = @[hvc, rvc];
