d0bb148ced8d5c6e8a87ae519c32d2388aca2c03
Matias <matiseijas@gmail.com>
2015-03-03 15:48:35 -0500

    Chapter 8 - In Progress

---
+++ textbook_work/matias_seijas/Chapter 8/Homepwner/BNRItemStore.h
@@ -0,0 +1,20 @@
+//
+//  BNRItemStore.h
+//  Homepwner
+//
+//  Created by Matias on 3/3/15.
+//  Copyright (c) 2015 Matias. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class BNRItem;
+
+@interface BNRItemStore : NSObject
+
+@property (nonatomic, readonly) NSArray *allItems;
+
++ (instancetype) sharedStore;
+- (BNRItem *) createItem;
+
+@end
--- textbook_work/matias_seijas/Chapter 8/Homepwner/BNRItemsViewController.m
+++ textbook_work/matias_seijas/Chapter 8/Homepwner/BNRItemsViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "BNRItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
 
 @implementation BNRItemsViewController
 
@@ -14,12 +17,42 @@
 {
     // Call the superclass's designated initializer
     self = [super initWithStyle:UITableViewStylePlain];
+    
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[BNRItemStore sharedStore] createItem];
+        }
+    }
+    
     return self;
 }
 
 - (instancetype) initWithStyle:(UITableViewStyle)style
 {
     return [self init];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Create an instance of UITableViewCell, with default appearance
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                   reuseIdentifier:@"UITableViewCell"];
+    
+    // Set the text on the cell with the description of the item
+    // this is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    BNRItem *item = items[indexPath.row];
+    
+    cell.textLabel.text = [item description];
+    
+    return cell;
 }
 
 @end
