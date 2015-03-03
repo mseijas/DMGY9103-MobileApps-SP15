db814b47e2f2646e5fc257f654c35078cd8d46cb
Matias <matiseijas@gmail.com>
2015-03-03 13:17:44 -0500

    Chapter 7 - Silver Challenge - In Progress

---
+++ textbook_work/matias_seijas/Chapter 7 - Silver Challenge/Hypnotizer/Hypnotizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnotizer
+//
+//  Created by Matias Seijas on 2/7/15.
+//  Copyright (c) 2015 Matias Seijas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
