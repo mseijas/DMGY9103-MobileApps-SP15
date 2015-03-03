a8e745137e4cf6e5af66e4bad36e09bf6f0c847e
Matias <matiseijas@gmail.com>
2015-03-03 16:25:52 -0500

    Chapter 8 - Challenges - In Progress

---
+++ textbook_work/matias_seijas/Chapter 8 - Challenges/Homepwner/BNRItemsViewController.m
@@ -0,0 +1,74 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by Matias on 3/3/15.
+//  Copyright (c) 2015 Matias. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+
+@implementation BNRItemsViewController
+
+- (instancetype) init
+{
+    // Call the superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[BNRItemStore sharedStore] createItem];
+        }
+    }
+    
+    return self;
+}
+
+- (instancetype) initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+
+- (UITableViewCell *) tableView:(UITableView *)tableView
+          cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Create an instance of UITableViewCell, with default appearance
+//    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+//                                                   reuseIdentifier:@"UITableViewCell"];
+    
+    // Get a new or recycled cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                                            forIndexPath:indexPath];
+    
+    // Set the text on the cell with the description of the item
+    // this is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    BNRItem *item = items[indexPath.row];
+    
+    cell.textLabel.text = [item description];
+    
+    return cell;
+}
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
+}
+
+
+
+@end
