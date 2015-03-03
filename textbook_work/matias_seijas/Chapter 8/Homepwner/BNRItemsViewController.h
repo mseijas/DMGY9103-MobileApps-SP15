79b37c9de05021c8165bd2573c393f8f8411fa61
Matias <matiseijas@gmail.com>
2015-03-03 13:38:47 -0500

    Chapter 8 - In Progress

---
+++ textbook_work/matias_seijas/Chapter 8/Homepwner/BNRItemsViewController.h
@@ -0,0 +1,15 @@
+//
+//  BNRItemsViewController.h
+//  Homepwner
+//
+//  Created by Matias on 3/3/15.
+//  Copyright (c) 2015 Matias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemsViewController : UITableViewController
+
+@end
+
+
---
+++ textbook_work/matias_seijas/Chapter 8/Homepwner/BNRItemsViewController.m
@@ -0,0 +1,25 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by Matias on 3/3/15.
+//  Copyright (c) 2015 Matias. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+- (instancetype) init
+{
+    // Call the superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+- (instancetype) initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+@end
