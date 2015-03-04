5210e21fe958ad735cb6d1b7bd2d3349d1180151
Matias Seijas <matiseijas@gmail.com>
2015-03-03 21:08:51 -0500

    Chapter 10 - In Progress

--- textbook_work/matias_seijas/Chapter 10/Homepwner/BNRDetailViewController.m
+++ textbook_work/matias_seijas/Chapter 10/Homepwner/BNRDetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BNRDetailViewController.h"
+#import "BNRItem.h"
 
 @interface BNRDetailViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
@@ -18,5 +19,27 @@
 
 
 @implementation BNRDetailViewController
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    BNRItem *item = self.item;
+    
+    self.nameField.text = item.itemName;
+    self.serialNumberField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d", item.valueInDollars];
+    
+    // You need an NSDateFormatter that will turn a date into a simple date string
+    static NSDateFormatter *dateFormatter;
+    if (!dateFormatter) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    
+    // Use filtered NSDate object to set dateLabel contents
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+}
 
 @end
--- textbook_work/matias_seijas/Chapter 10/Homepwner/BNRItemsViewController.m
+++ textbook_work/matias_seijas/Chapter 10/Homepwner/BNRItemsViewController.m
@@ -169,6 +169,12 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     BNRDetailViewController *detailViewController = [[BNRDetailViewController alloc] init];
     
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    BNRItem *selectedItem = items[indexPath.row];
+    
+    // Give detail view controller a pointer to the item object in row
+    detailViewController.item = selectedItem;
+    
     // Push it to the top of the navigation controller's stack
     [self.navigationController pushViewController:detailViewController
                                          animated:YES];
