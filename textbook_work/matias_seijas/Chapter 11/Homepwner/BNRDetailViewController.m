207886f3b2bb8bdd2acbefe94f89347554d5e9ca
Matias Seijas <matiseijas@gmail.com>
2015-03-08 17:22:21 -0400

    Chapter 11 - In Progress

---
+++ textbook_work/matias_seijas/Chapter 11/Homepwner/BNRDetailViewController.m
@@ -0,0 +1,74 @@
+//
+//  BNRDetailViewController.m
+//  Homepwner
+//
+//  Created by Matias Seijas on 3/3/15.
+//  Copyright (c) 2015 Matias. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+#import "BNRItem.h"
+
+@interface BNRDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
+
+
+@end
+
+
+@implementation BNRDetailViewController
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    BNRItem *item = self.item;
+    
+    self.nameField.text = item.itemName;
+    self.serialNumberField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d", item.valueInDollars];
+    
+    // You need an NSDateFormatter that will turn a date into a simple date string
+    static NSDateFormatter *dateFormatter;
+    if (!dateFormatter) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    
+    // Use filtered NSDate object to set dateLabel contents
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    // Clear first responder
+    [self.view endEditing:YES];
+    
+    // "Save" changes to item
+    BNRItem *item = self.item;
+    item.itemName = self.nameField.text;
+    item.serialNumber = self.serialNumberField.text;
+    item.valueInDollars = [self.valueField.text intValue];
+    
+}
+
+- (void) setItem:(BNRItem *)item
+{
+    _item = item;
+    self.navigationItem.title = _item.itemName;
+}
+
+- (IBAction)takePicture:(id)sender {
+    
+}
+
+
+@end
