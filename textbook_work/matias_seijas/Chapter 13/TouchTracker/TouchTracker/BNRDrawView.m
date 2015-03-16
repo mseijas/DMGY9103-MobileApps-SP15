6547223875e00dca19b2f75276754028ca444e51
Matias Seijas <matiseijas@gmail.com>
2015-03-16 19:30:13 -0400

    Chapter 13 - In Progress

--- textbook_work/matias_seijas/Chapter 13/TouchTracker/TouchTracker/BNRDrawView.m
+++ textbook_work/matias_seijas/Chapter 13/TouchTracker/TouchTracker/BNRDrawView.m
@@ -29,6 +29,12 @@
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                              action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        
+        [self addGestureRecognizer:doubleTapRecognizer];
     }
     
     return self;
@@ -119,5 +125,13 @@
     }
 }
 
+- (void)doubleTap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Double Tap");
+    
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+}
 
 @end
