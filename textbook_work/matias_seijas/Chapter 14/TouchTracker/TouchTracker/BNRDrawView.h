72fdb818b0646b3ae24c6b4571c0d675ea0cc378
Matias Seijas <matiseijas@gmail.com>
2015-03-17 20:13:39 -0400

    Chapter 14 - In Progress

---
+++ textbook_work/matias_seijas/Chapter 14/TouchTracker/TouchTracker/BNRDrawView.h
@@ -0,0 +1,15 @@
+//
+//  BNRDrawView.h
+//  TouchTracker
+//
+//  Created by Matias Seijas on 3/10/15.
+//  Copyright (c) 2015 Matias Seijas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface BNRDrawView : UIView
+
+@end
---
+++ textbook_work/matias_seijas/Chapter 14/TouchTracker/TouchTracker/BNRDrawViewController.h
@@ -0,0 +1,14 @@
+//
+//  BNRDrawViewController.h
+//  TouchTracker
+//
+//  Created by Matias Seijas on 3/10/15.
+//  Copyright (c) 2015 Matias Seijas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface BNRDrawViewController : UIViewController
+
+@end
