15b99677a3f35dc10ce7db8bfecfb1ebbf930a5f
Matias Seijas <matiseijas@gmail.com>
2015-03-28 13:19:42 -0400

    Chapter 17 - Bronze Challenge

--- textbook_work/matias_seijas/Chapter 17/Homepwner/BNRItemStore.m
+++ textbook_work/matias_seijas/Chapter 17/Homepwner/BNRItemStore.m
@@ -27,9 +27,14 @@
     static BNRItemStore *sharedStore = nil;
     
     // Do I need to create a sharedStore?
-    if (!sharedStore) {
+//    if (!sharedStore) {
+//        sharedStore = [[self alloc] initPrivate];
+//    }
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
         sharedStore = [[self alloc] initPrivate];
-    }
+    });
     
     return sharedStore;
 }
